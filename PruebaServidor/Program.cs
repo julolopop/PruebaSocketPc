f884e064320aa01e69adcf07af72ca3eedfe80ca
julolopop <jjm5@hotmail.es>
2018-02-03 19:10:30 +0100

    sokect c shar

---
+++ PruebaServidor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PruebaServidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Socket miPrimerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+           
+            IPEndPoint miDireccion = new IPEndPoint(IPAddress.Parse("192.168.1.40"), 1234);
+            
+            try
+            {
+                // paso 4
+                miPrimerSocket.Bind(miDireccion); 
+                miPrimerSocket.Listen(1); 
+                Console.WriteLine("Escuchando...");
+                Socket Escuchar = miPrimerSocket.Accept();
+                byte[] msg = new byte[255];
+                Escuchar.Receive(msg);
+                Console.WriteLine(Encoding.ASCII.GetString(msg));
+             
+                Console.WriteLine("Conectado con exito");
+
+
+
+               
+                miPrimerSocket.Close(); 
+            }
+            catch (Exception error)
+
+            {
+                Console.WriteLine("Error: {0}", error.ToString());
+            }
+
+            Console.WriteLine("Presione cualquier tecla para terminar");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ PruebaSocketPc/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace PruebaSocketPc
+{
+    class Program
+    {
+        
+
+
+        static void Main(string[] args)
+        {
+            Socket miPrimerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+           
+
+            IPEndPoint miDireccion = new IPEndPoint(IPAddress.Parse("192.168.1.40"), 1234);
+            
+          
+            try
+
+            {
+                
+                miPrimerSocket.Connect(miDireccion);    
+                
+                Console.WriteLine("Conectado con exito");
+
+                byte[] msg = new byte[255];
+                msg = Encoding.ASCII.GetBytes("Hola y adiso");
+                miPrimerSocket.Send(msg);
+
+                miPrimerSocket.Close();
+                
+            }
+            
+            catch (Exception error)
+
+            {
+                
+                Console.WriteLine("Error: {0}", error.ToString());
+                
+            }
+            
+            Console.WriteLine("Presione cualquier tecla para terminar");
+            
+            Console.ReadLine();
+
+
+
+
+
+            }
+
+        }
+}
